c72c0dc29e66763e4f8e06961baa28c812fc6118
JoseFelixeng <jfelixnunes2k15@gmail.com>
2024-07-31 23:10:32 -0300

    add att

--- api/main.py
+++ api/main.py
@@ -16,4 +16,16 @@ DB_CONFIG = {
 }
 
 
+class administrativo(BaseModel):
+    id: int
+    nome: str
+    matricula: str
+    id_usuario: int 
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union [str, None] = None ):
+    return {"item_id" : item.name}
